e996833f8e48649c362b7fce417f7c02b34246b7
shivasriram <108151270+shivasriram@users.noreply.github.com>
2023-09-04 22:53:11 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Assignment 0
+
+You should edit the file "answer.py" and answer the questions asked.
+
+Please don't try to rename the files.
+Please don't try to edit other files.
+
+## Make sure your code is bugs free and ready to run
+Test your code before submit.
+## Good luck
---
+++ answer.py
@@ -0,0 +1,18 @@
+def hello_world():
+    # Write some thing in the following doc string
+    """
+
+    """
+
+    # Comment the following line
+   # Comment this line
+
+    # Print "Hello, World"
+    print("A")
+
+def add(a,b):
+
+    # Write simple code to add variable a and b
+    output=
+
+    return output
